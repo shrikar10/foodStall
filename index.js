5aca3d100256d0454e4c30c4306ac75cab930974
shrikar10 <shrikark7@gmail.com>
2023-01-19 10:34:33 +0530

    email sending function

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log("Hello");
+const nodemailer = require("nodemailer");
+const express = require("express");
+require("dotenv").config();
+
+//express
+const app = express();
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  auth: {
+    user: process.env.USER,
+    pass: process.env.PASS,
+  },
+  secure: true,
+});
+
+app.post("/send-email", (req, res) => {
+  // code to send the email goes here
+  transporter.sendMail(
+    {
+      from: "shivarajum42@gmail.com",
+      to: "shivajyothiachar42@gmail.com",
+      subject: "Hello World thank you for subscribing",
+      text: "Regards Raju",
+    },
+    (error, info) => {
+      if (error) {
+        res.status(500).send(error);
+      } else {
+        res.status(200).send("Email sent: " + info.response);
+      }
+    }
+  );
+  console.log("Mail sent");
+});
+
+const port = 8000;
+app.listen(port);
