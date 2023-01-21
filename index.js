66e8d51dc81ad69a60f6c7cf951162755425a4af
shrikar C Kulkarni <60685603+shrikar10@users.noreply.github.com>
2023-01-21 12:48:09 +0530

    email sending function (#1)
    * email sending function
    
    * Updated To email and message
    
    * added swagger docs
    
    Co-authored-by: shivaraju <shivarajum39@gmail.com>

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-console.log("Hello");
+const nodemailer = require("nodemailer");
+const express = require("express");
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+require("dotenv").config();
+
+//express
+const app = express();
+
+//middleware
+app.use(express.json());
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  auth: {
+    user: process.env.USER,
+    pass: process.env.PASS,
+  },
+  secure: true,
+});
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.post("/send-email", (req, res) => {
+  // console.log(req.body.email);
+  // code to send the email goes here
+  transporter.sendMail(
+    {
+      from: process.env.USER,
+      to: req.body.email,
+      subject: "Hello World thank you for subscribing",
+      text: req.body.message,
+    },
+    (error, info) => {
+      if (error) {
+        res.status(500).send(error);
+      } else {
+        res.status(200).send("Email sent: " + info.response);
+      }
+    }
+  );
+});
+
+const port = 8000;
+app.listen(port);
